826ae51e6c6c901b6b99c306b5fe4f528d12480b
hemantc09 <hemantc09@gmail.com>
2018-11-18 14:27:31 -0800

    added server and port adn set up production build and server

---
+++ readme.md
@@ -0,0 +1,8 @@
+#git commands
+
+
+git init - create nw repo
+git status  - changes to the code
+git add - actually add the files to the staging area
+git commit - create a git commit
+git log - view recent commit
--- src/app.js
+++ src/app.js
@@ -7,7 +7,10 @@ import IndecesionApp from './components/IndecesionApp'
 import 'normalize.css/normalize.css';
 //load the css file
 import './styles/styles.scss';
-
+const pot = process.env.PORT || 3000;
 
 ReactDOM.render(<IndecesionApp />,document.getElementById('app'));
 
+app.listen(port,() =>{
+    console.log("server is up");
+});
